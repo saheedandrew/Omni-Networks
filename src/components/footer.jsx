3088346c28b49fa30c56facb97702139607a8f33
saheedandrew <andrewsaheed@gmail.com>
2023-12-21 18:01:13 -0500

    Initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Our Services</h2>
+          <p>
+          Omni Networks is committed to providing equal employment opportunities to all qualified individuals without regard to race, color, religion, sex, national origin, age, disability, sexual orientation, gender identity, or any other characteristic protected by applicable laws.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
